749224ae2cd6ba592a4e85f309dbd7c8fd98ac51
Khin Liew <khinl@student.unimelb.edu.au>
2023-02-18 14:20:23 +1100

    table size update fix

--- script.js
+++ script.js
@@ -10,40 +10,44 @@ var billDivider = 1;
 billAmount.addEventListener('input', updateBillAmount);
 tableSize.addEventListener('input', updateTableSize);
 
-tipPercentages.forEach(tipPercentages => {
-  tipPercentages.addEventListener('change', updateSelected);
+tipPercentages.forEach(tipPercentageCheckBox => {
+  tipPercentageCheckBox.addEventListener('change', updateSelected);
 });
 customPercentage.addEventListener('input', updateCustom);
 
 function updateBillAmount() {
-  calculateTipAmount(tipPercentage);
+  calculateTipAmount();
 }
 
 function updateSelected() {
-  let tipPercentage = document.querySelector("input[name='tip-percentage']:checked").value;
+  tipPercentage = document.querySelector("input[name='tip-percentage']:checked").value;
   customPercentage.value = 0;
-  calculateTipAmount(tipPercentage);
+  calculateTipAmount();
 }
 
 function updateCustom() {
   tipPercentage = customPercentage.value;
   // document.querySelector("input[name='tip-percentage']:checked").checked = false;
-  calculateTipAmount(tipPercentage);
+  calculateTipAmount();
 }
 
 function updateTableSize() {
   billDivider = tableSize.value || 1;
-  calculateTipAmount(tipPercentage);
+  calculateTipAmount();
 }
 
-function calculateTipAmount(tipPercentage) {
+function calculateTipAmount() {
   let tipAmount = (billAmount.value*tipPercentage/100)/billDivider;
   let bill = parseFloat(billAmount.value) || 0;
   let totalAmount = (bill + tipAmount*billDivider)/billDivider;
-  document.getElementById("tip-amount").textContent = tipAmount;
-  document.getElementById("total-amount").textContent = totalAmount;
+  document.getElementById("tip-amount").textContent = parseFloat(tipAmount).toFixed(2);
+  document.getElementById("total-amount").textContent = parseFloat(totalAmount).toFixed(2);
+}
+
+function uncheck() {
+  
 }
 
 function reset() {
-  billAmount = 0.00;
+  
 };
